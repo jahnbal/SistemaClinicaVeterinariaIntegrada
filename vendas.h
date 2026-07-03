8f4749c8fd58dad3943f582593a8abdd28b4cf5b
jahnbal <silveirarthurburner@gmail.com>
2026-07-03 07:42:52 -0300

    Delete vendas.h

--- vendas.h
+++
@@ -1,9 +0,0 @@
-#ifndef VENDAS_H
-#define VENDAS_H
-#include "produtos.h"
-
-int RealizarVenda(Produto produto);
-void RegistrarVenda(Produto produto);
-int BuscarVenda(Produto produto, FILE *arq);
-
-#endif
